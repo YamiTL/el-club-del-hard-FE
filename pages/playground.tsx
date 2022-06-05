aeee8fb3ab3a617b35be3116272db445a01289ba
Berttah <nachebertah@gmail.com>
2022-06-04 22:13:02 -0300

    Component Label Stock added

--- pages/playground.tsx
+++ pages/playground.tsx
@@ -1,9 +1,11 @@
 import { NextPage } from 'next';
 import LabelPromo from '../components/LabelPromo/LabelPromo';
+import LabelStock from '../components/LabelStock/LabelStock';
 const playground: NextPage = () => {
   return (<>
       <h1>playground</h1>
       <LabelPromo text={"En Oferta"} style={"onSale"} disabled={false} />
+      <LabelStock text={"Stock"} style={"onStock"} disabled={false} />
     </>
   )
 }
