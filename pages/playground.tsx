58617bacc8612f7f7f1bf2d55438c9b074191988
Berttah <nachebertah@gmail.com>
2022-06-05 17:18:56 -0300

    arregle los errores mencionados

--- pages/playground.tsx
+++ pages/playground.tsx
@@ -1,13 +1,13 @@
 import { NextPage } from 'next';
 import LabelPromo from '../components/LabelPromo/LabelPromo';
 import LabelStock from '../components/LabelStock/LabelStock';
-import MainNavBarButton from '../components/MainNavBarButton/index';
+import ButtonMainNavBar from '../components/ButtonMainNavBar/index';
 const playground: NextPage = () => {
   return (<>
       <h1>playground</h1>
       <LabelPromo text={"En Oferta"} style={"onSale"} disabled={false} />
       <LabelStock text={"Stock"} style={"onStock"} disabled={false} />
-      <MainNavBarButton text={"Categories or Help"} style={"MainNavBarButton"} disabled={false} onClick={Function} />
+      <ButtonMainNavBar text={"Categories or Help"} style={"mainNavBarButton"} disabled={false} onClick={Function} />
     </>
   )
 }
