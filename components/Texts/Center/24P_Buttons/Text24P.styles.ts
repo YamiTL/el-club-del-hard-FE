a65f6cba593773ae505615b5ad7446804d7ee18d
YamiTL <yamitlemos@gmail.com>
2022-06-18 21:29:26 -0300

    Created Text20 and Text24 components, still working on BannerImage Container

---
+++ components/Texts/Center/24P_Buttons/Text24P.styles.ts
@@ -0,0 +1,15 @@
+import { style } from 'typestyle';
+
+const styles = {
+  text: style({
+    fontSize: '24px',
+    fontFamily: 'Roboto',
+    style: 'normal',
+    fontWeight: '400',
+    lineHeight: '28px',
+    textAlign: 'center',
+    color: '#000000',
+  }),
+};
+
+export default styles;
